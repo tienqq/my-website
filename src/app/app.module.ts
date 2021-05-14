f6f4c5a98ebec5109304a7373d663653e3088513
Tien Quang <t.quang45@gmail.com>
2021-05-14 13:53:17 -0500

    Revert "gonna revert"
    This reverts commit aab5ac3d6ff69ab7ef1373a4c43ca550f8b3a6e3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,13 @@ import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { AlertComponent } from './ticket_demo/_components/alert/alert.component';
-import { LayoutComponent } from './ticket_demo/account/layout/layout.component';
-import { LoginComponent } from './ticket_demo/account/login/login.component';
-import { RegisterComponent } from './ticket_demo/account/register/register.component';
-import { AddEditComponent } from './ticket_demo/users/add-edit/add-edit.component';
-import { ListComponent } from './ticket_demo/users/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent,
-    AlertComponent,
-    LayoutComponent,
-    LoginComponent,
-    RegisterComponent,
-    AddEditComponent,
-    ListComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
