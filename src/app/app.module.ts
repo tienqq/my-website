b5ba26c161810b3eac5f3587dd7b885aa3be0f1a
Tien Quang <t.quang45@gmail.com>
2021-05-14 13:15:19 -0500

    Revert "Added a footer and home component"
    This reverts commit 51c2ddfc10e06092499c48215e0cbfe44be5acc3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
